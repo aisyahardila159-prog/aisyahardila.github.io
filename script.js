dd2edd3e5739d48ee51817473c9c2a8f72d575b3
Aisyah.A <aisyahardila159@gmail.com>
2025-09-19 08:43:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+  function toggleDetail(card) {
+         let detail = card.querySelector(".school-detail");
+        detail.style.display = detail.style.display === "block" ? "none" : "block";
+    }
+               
